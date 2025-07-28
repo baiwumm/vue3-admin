34e7fd3d5cbd90e98351d16c6966e92b2a90c3c5
白雾茫茫丶 <baiwumm@foxmail.com>
2025-07-28 09:22:33 +0800

    feat: 初始化仓库

---
+++ src/config/winston.config.ts
@@ -0,0 +1,71 @@
+/*
+ * @Author: 白雾茫茫丶<baiwumm.com>
+ * @Date: 2024-08-29 16:41:59
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2025-07-24 16:20:28
+ * @Description: winston 日志
+ */
+import chalk from 'chalk'; // 用于颜色化输出
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+// 定义日志级别颜色
+const levelsColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+  verbose: 'cyan',
+};
+
+const winstonLogger = createLogger({
+  format: format.combine(format.timestamp(), format.errors({ stack: true }), format.splat(), format.json()),
+  defaultMeta: { service: 'log-service' },
+  transports: [
+    new DailyRotateFile({
+      filename: 'logs/errors/error-%DATE%.log', // 日志名称，占位符 %DATE% 取值为 datePattern 值。
+      datePattern: 'YYYY-MM-DD', // 日志轮换的频率，此处表示每天。
+      zippedArchive: true, // 是否通过压缩的方式归档被轮换的日志文件。
+      maxSize: '20m', // 设置日志文件的最大大小，m 表示 mb 。
+      maxFiles: '14d', // 保留日志文件的最大天数，此处表示自动删除超过 14 天的日志文件。
+      level: 'error', // 日志类型，此处表示只记录错误日志。
+    }),
+    new DailyRotateFile({
+      filename: 'logs/warnings/warning-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      level: 'warn',
+    }),
+    new DailyRotateFile({
+      filename: 'logs/app/app-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    new transports.Console({
+      format: format.combine(
+        format.colorize({
+          colors: levelsColors,
+        }),
+        format.simple(),
+        format.printf((info) => {
+          const symbols = Object.getOwnPropertySymbols(info) as [symbol, symbol, symbol];
+          const levelSymbol = symbols[0];
+          const messageSymbol = symbols[2];
+
+          // 断言 levelsColors 的键类型
+          const color = levelsColors[info[levelSymbol] as keyof typeof levelsColors];
+          const chalkColor = chalk[color];
+
+          return `${chalkColor(info.timestamp)} ${chalkColor(info[messageSymbol])}`;
+        }),
+      ),
+      level: 'debug',
+    }),
+  ],
+});
+
+export default winstonLogger;
